022da03a2dd724f30828d4e55a1c989f8b8c4c90
DaniilDvoretskiy <158847641+DaniilDvoretskiy@users.noreply.github.com>
2024-07-07 13:59:23 +0300

    Нужно больше функций

---
+++ js/function.js
@@ -0,0 +1,32 @@
+// Функция для проверки длины строки.
+const regularString = (inputString, maxLength) => inputString.length <= maxLength;
+
+regularString ('Hello', 5);
+regularString ('Goodbye', 3);
+
+// Функция для проверки, является ли строка палиндромом.
+const stringPalindrome = (string) => {
+    const normalizedString = string.replaceAll(' ', '').toLowerCase();
+    const reversedString = normalizedString.split('').reverse().join('');
+    return normalizedString === reversedString;
+  };
+  
+  stringPalindrome('Уверен и не реву');
+  stringPalindrome('Коту скоро сорок суток');
+  stringPalindrome('Академия');
+
+// Функия для извлечения цифр
+const extractingNumbers = (inputString) => {
+  const numbers = inputString.match(/\d+/g);
+  if (numbers === null) {
+    return NaN;
+  } else {
+    return parseInt(numbers.join(''));
+  }
+}
+  
+extractingNumbers('2023 год');
+extractingNumbers('ECMAScript 2022');
+extractingNumbers('1 кефир, 0.5 батона');
+extractingNumbers('агент 007'); 
+extractingNumbers('а я томат');
