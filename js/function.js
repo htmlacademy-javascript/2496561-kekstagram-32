9bb199b3327ad2f1f59d7bc6c5ac6b14b0424930
DaniilDvoretskiy <158847641+DaniilDvoretskiy@users.noreply.github.com>
2024-07-07 14:10:23 +0300

    Убирает пробелы и добавляет второй параметр для функции parseInt

--- js/function.js
+++ js/function.js
@@ -6,14 +6,14 @@ regularString ('Goodbye', 3);
 
 // Функция для проверки, является ли строка палиндромом.
 const stringPalindrome = (string) => {
-    const normalizedString = string.replaceAll(' ', '').toLowerCase();
-    const reversedString = normalizedString.split('').reverse().join('');
-    return normalizedString === reversedString;
-  };
-  
-  stringPalindrome('Уверен и не реву');
-  stringPalindrome('Коту скоро сорок суток');
-  stringPalindrome('Академия');
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  const reversedString = normalizedString.split('').reverse().join('');
+  return normalizedString === reversedString;
+};
+
+stringPalindrome('Уверен и не реву');
+stringPalindrome('Коту скоро сорок суток');
+stringPalindrome('Академия');
 
 // Функия для извлечения цифр
 const extractingNumbers = (inputString) => {
@@ -21,12 +21,12 @@ const extractingNumbers = (inputString) => {
   if (numbers === null) {
     return NaN;
   } else {
-    return parseInt(numbers.join(''));
+    return parseInt(numbers.join(''), 10);
   }
-}
-  
+};
+
 extractingNumbers('2023 год');
 extractingNumbers('ECMAScript 2022');
 extractingNumbers('1 кефир, 0.5 батона');
-extractingNumbers('агент 007'); 
+extractingNumbers('агент 007');
 extractingNumbers('а я томат');
