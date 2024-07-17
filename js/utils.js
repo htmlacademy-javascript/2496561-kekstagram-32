34b1c4ab455e523222a5d31573aba695afcb1068
DaniilDvoretskiy <158847641+DaniilDvoretskiy@users.noreply.github.com>
2024-07-17 19:55:29 +0300

    Модуляция

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,14 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export {getRandomInteger, getRandomArrayElement};
+
 // Функция для проверки длины строки.
 const regularString = (inputString, maxLength) => inputString.length <= maxLength;
 
