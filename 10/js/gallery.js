279c67ba32dc374155b3a288f55249667f58cf74
Keks <keks@htmlacademy.ru>
2024-08-01 15:51:10 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/gallery.js
@@ -0,0 +1,20 @@
+import {generateThumbnails} from './thumbnail.js';
+import {showBigPicture} from './big-pictures.js';
+
+const container = document.querySelector('.pictures');
+
+const renderGallery = (pictures) => {
+  container.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('[data-thumbnail-id]');
+    if (!thumbnail) {
+      return;
+    }
+    evt.preventDefault();
+    const picture = pictures.find(
+      (item) => item.id === +thumbnail.dataset.thumbnailId
+    );
+    showBigPicture(picture);
+  });
+  generateThumbnails(pictures, container);
+};
+export {renderGallery};
